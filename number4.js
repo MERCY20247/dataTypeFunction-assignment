90de0907ce5bc9427b675ed784aef4b0dea3778c
nasejje mariam lumala <aziirwemercy@gmail.com>
2025-02-12 14:17:58 +0300

    my changes

---
+++ number4.js
@@ -0,0 +1,11 @@
+//  What is the typeof operator used for? Provide two examples.
+// The typeof operator in JavaScript is used to determine the data type of a given value or variable.
+//  It returns a string that represents the type of the operand.
+// Examples
+// 1. Checking the type of a string.
+let message = "Hello students";
+console.log(typeof message);
+
+// 2. Checking the type of a number.
+let age = 25;
+console.log(typeof age);
